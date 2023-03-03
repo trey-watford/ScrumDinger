1e3f1a42bcab5e6b88d98e0dbe905c4303d2c0fc
trey watford <twatford@employreward.com>
2023-03-03 08:58:43 -0500

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Trey Watford on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
