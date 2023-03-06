65d25f35e5b9785c24e0144bdc20ce33c145d3ae
trey watford <twatford@employreward.com>
2023-03-06 14:28:45 -0500

    finished introduction to swift ui using basic elements

--- ScrumDinger/MeetingView.swift
+++ ScrumDinger/MeetingView.swift
@@ -8,28 +8,44 @@
 import SwiftUI
 
 struct MeetingView: View {
-//    starting state of progress bar
+    //    starting state of progress bar
     @State private var progress = 5.0
-    
+
     var body: some View {
         VStack {
             ProgressView(value: progress, total: 15)
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
-                VStack{
-                    Text("Seconds Remaining")
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            
             Button("More") {
                 progress += 1.0
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            
+            Circle().strokeBorder(lineWidth: 24)
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
         }
         .padding()
-        
     }
 }
 
